f31a510abba32230ffc057cfd19d864a12c78f1f
nguyenthaiduc <89315482+Melody0212@users.noreply.github.com>
2022-01-20 21:16:58 +0700

    add page

---
+++ pages/works.js
@@ -0,0 +1,27 @@
+import NextLink from 'next/link'
+import {
+  Box,
+  Heading,
+  Text,
+  Container,
+  Divider,
+  Button
+} from '@chakra-ui/react'
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Heading as="h1">404</Heading>
+      <Text>Không tìm thấy trang vui lòng quay lại trang chủ !</Text>
+      <Divider my={6} />
+
+      <Box my={6} align="center">
+        <NextLink href="/">
+          <Button colorScheme="teal">Về Trang Chủ</Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
+}
+
+export default NotFound
